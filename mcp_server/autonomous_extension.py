cec8bf08fb216ca1854b3154784f09d5d2e0c8fe
Luigi Di Carlo <ing.luigidicarlo@gmail.com>
2025-09-27 15:44:37 +0200

    fatto la da luigi Funzionante graphit, autonomus, docling.

---
+++ mcp_server/autonomous_extension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Autonomous Extension Stub - Compatibility layer
+"""
+
+class ComplianceViolation:
+    """Stub for ComplianceViolation"""
+    pass
+
+class GovernanceDocument:
+    """Stub for GovernanceDocument"""
+    pass
+
+def initialize_autonomous_engine(*args, **kwargs):
+    """Stub function that accepts any parameters"""
+    pass
+
+def get_autonomous_engine():
+    """Stub function"""
+    return None
